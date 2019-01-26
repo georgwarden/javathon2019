5e0eae41f77e23567680b6988e92a87842aa38eb
George Warden <georgwarden@gmail.com>
2019-01-26 19:59:10 +0300

    create Result monad

---
+++ src/main/java/ru/mai/pubstash/util/Result.java
@@ -0,0 +1,52 @@
+package ru.mai.pubstash.util;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public interface Result<T> {
+
+    <R> R fold(Function<T, R> ifSuccess, Supplier<R> ifFail);
+    <N> Result<N> map(Function<T, N> mapper);
+
+    static <T> Result<T> success(T value) {
+        return new Success<>(value);
+    }
+
+    static <T> Result<T> fail() {
+        return new Failure<>();
+    }
+
+}
+
+class Success<T> implements Result<T> {
+
+    public final T value;
+
+    Success(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public <R> R fold(Function<T, R> ifSuccess, Supplier<R> ifFail) {
+        return ifSuccess.apply(value);
+    }
+
+    @Override
+    public <N> Result<N> map(Function<T, N> mapper) {
+        return new Success<>(mapper.apply(value));
+    }
+}
+
+class Failure<T> implements Result<T> {
+
+    @Override
+    public <R> R fold(Function<T, R> ifSuccess, Supplier<R> ifFail) {
+        return ifFail.get();
+    }
+
+    @Override
+    public <N> Result<N> map(Function<T, N> mapper) {
+        return new Failure<>();
+    }
+
+}
