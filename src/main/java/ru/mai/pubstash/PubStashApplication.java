1dd6c294a990547572b289dbeb6f68fbf72535e8
George Warden <georgwarden@gmail.com>
2019-01-26 17:51:54 +0300

    init commit

---
+++ src/main/java/ru/mai/pubstash/PubStashApplication.java
@@ -0,0 +1,14 @@
+package ru.mai.pubstash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubStashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubStashApplication.class, args);
+	}
+
+}
+
